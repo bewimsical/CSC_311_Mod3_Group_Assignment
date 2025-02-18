1bc0453d177867bfeb8e4bc8b1fa5bcf694112e8
darianne123 <darianneramos@icloud.com>
2025-02-18 10:51:37 -0500

    added in the variables to MazeController

--- src/main/java/edu/farmingdale/csc_311_mod3_group_assignment/MazeController.java
+++ src/main/java/edu/farmingdale/csc_311_mod3_group_assignment/MazeController.java
@@ -1,14 +1,34 @@
 package edu.farmingdale.csc_311_mod3_group_assignment;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 public class MazeController {
     @FXML
-    private Label welcomeText;
+    private Button restartMaze1;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button restartMaze2;
+
+    @FXML
+    private Button solveMaze1;
+
+    @FXML
+    private Button solveMaze2;
+
+    @FXML
+    private RadioButton carButton1;
+
+    @FXML
+    private RadioButton carButton2;
+
+    @FXML
+    private RadioButton robotButton1;
+
+    @FXML
+    private RadioButton robotButton2;
+
+
 }
