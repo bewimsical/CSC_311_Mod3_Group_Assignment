b393bb174524fa3553899508fb9416c4052654b5
Kathleen Wims <bewimsical@gmail.com>
2025-02-18 10:39:02 -0500

    added sprite class

---
+++ src/main/java/edu/farmingdale/csc_311_mod3_group_assignment/MovableSprite.java
@@ -0,0 +1,14 @@
+package edu.farmingdale.csc_311_mod3_group_assignment;
+
+public class MovableSprite {
+    int dx;
+    int dy;
+
+    public MovableSprite(){
+        super();
+        dx = 0;
+        dy = 0;
+
+
+    }
+}
